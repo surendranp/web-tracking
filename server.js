c5f3f037cd9f0f90025384b04cd1ccf0f7f94e62
suren67 <surendranpandi02@gmail.com>
2024-09-02 15:42:11 +0530

    test_15.42

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// server.js
+
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -13,8 +15,11 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
+// Log MongoDB URI for debugging
+console.log('MongoDB URI:', process.env.MONGO_URI);
+
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log('MongoDB connection error:', err));
 
