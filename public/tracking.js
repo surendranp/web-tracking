20d142b36da9031a4084b9140d64b69974e18409
suren67 <surendranpandi02@gmail.com>
2024-09-12 10:46:36 +0530

    12.09.2024_test_10.46

--- public/tracking.js
+++ public/tracking.js
@@ -1,5 +1,5 @@
 (function() {
-  const trackingUrl = 'https://web-tracking-production.up.railway.app/api/pageviews';  // Replace with your actual API URL
+  const trackingUrl = 'https://web-tracking-production.up.railway.app/api/track';  // Replace with your actual API URL
 
   async function getUserIP() {
     try {
@@ -18,20 +18,22 @@
 
   async function sendTrackingData(data) {
     const ip = await getUserIP();
-    fetch(trackingUrl, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ ...data, ip })
-    }).catch(error => console.error('Error sending tracking data:', error.message));
+    const domain = window.location.hostname; // Get the domain name
+    try {
+      await fetch(trackingUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ ...data, ip, domain })
+      });
+    } catch (error) {
+      console.error('Error sending tracking data:', error.message);
+    }
   }
 
   let sessionId = localStorage.getItem('sessionId') || generateSessionId();
   localStorage.setItem('sessionId', sessionId);
-
-  // Extract domain or identifier
-  const siteIdentifier = window.location.hostname;
 
   // Track page view
   function trackPageView() {
@@ -39,8 +41,7 @@
       type: 'pageview',
       url: window.location.href,
       timestamp: new Date().toISOString(),
-      sessionId,
-      siteIdentifier
+      sessionId
     });
   }
 
@@ -49,24 +50,24 @@
   // Track click events
   document.addEventListener('click', function(event) {
     let elementName = 'Unnamed Element';
-    let elementType = '';
 
     if (event.target.tagName === 'BUTTON') {
       elementName = event.target.innerText || event.target.id || 'Unnamed Button';
-      elementType = 'button_click';
-    } else if (event.target.tagName === 'A') {
-      elementName = event.target.innerText || event.target.id || 'Unnamed Link';
-      elementType = 'link_click';
-    }
-
-    if (elementType) {
       sendTrackingData({
-        type: elementType,
-        [elementType === 'button_click' ? 'buttonName' : 'linkName']: elementName,
+        type: 'button_click',
+        buttonName: elementName,
         url: window.location.href,
         timestamp: new Date().toISOString(),
-        sessionId,
-        siteIdentifier
+        sessionId
+      });
+    } else if (event.target.tagName === 'A') {
+      elementName = event.target.innerText || event.target.id || 'Unnamed Link';
+      sendTrackingData({
+        type: 'link_click',
+        linkName: elementName,
+        url: window.location.href,
+        timestamp: new Date().toISOString(),
+        sessionId
       });
     }
   });
