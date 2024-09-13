ab1586b7ce013dcafc2a4191e606f4d17c29051e
suren67 <surendranpandi02@gmail.com>
2024-09-13 15:09:17 +0530

    _test_15.09

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ async function sendTrackingDataToClient(domain, email) {
     service: 'gmail',
     auth: {
       user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS
+      pass: process.env.EMAIL_PASS 
     }
   });
 
